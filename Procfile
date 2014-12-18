f4d27a86a53e6b8f8434c9042ce06ff0efec5b75
Sebastian Cheung <sebastian_cheung@yahoo.com>
2014-12-18 02:10:47 +0000

    modified Procfile definition

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --log-file - app:app
+web: gunicorn hello:app
--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+import os
 app = Flask(__name__)
 
 
@@ -9,4 +9,6 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run()
+    # app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
